fa483c0b4138667bb3911fc3609b1e4c537c1683
Harald Krekeler <mail@krekeler.de>
2021-03-05 14:03:38 +0100

    THREAD_PRIORITY consts for macOS

--- src/util.h
+++ src/util.h
@@ -54,7 +54,7 @@ static const bool DEFAULT_LOGIPS         = false;
 static const bool DEFAULT_LOGTIMESTAMPS  = true;
 static const bool DEFAULT_LOGTHREADNAMES = false;
 
-#ifdef __linux__
+#if defined(__linux__) || defined(__APPLE__)
 static const int THREAD_PRIORITY_LOWEST = 19;
 static const int THREAD_PRIORITY_NORMAL = 0;
 #endif
